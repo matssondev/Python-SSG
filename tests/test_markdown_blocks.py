efc5e9d8ca8edbc4ffa06007b23a51d958e5191b
matssondev <patric@matsson.dev>
2026-01-23 22:03:01 +0100

    Block to blocktype function with tests

--- tests/test_markdown_blocks.py
+++ tests/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.markdown_blocks import markdown_to_blocks
+from src.markdown_blocks import markdown_to_blocks, BlockType, block_to_block_type
 
 
 class TestMarkdownBlocks(unittest.TestCase):
@@ -38,6 +38,60 @@ This is the same paragraph on a new line
     def test_markdown_to_blocks_ignores_empty_blocks(self):
         md = "First\n\n\n\nSecond"
         self.assertEqual(markdown_to_blocks(md), ["First", "Second"])
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_headings(self):
+        block = "# Heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+        block = "#Heading"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_code_blocks(self):
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+
+        block = "```\ncode"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_quotes(self):
+        block = "> quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+
+        block = "> quote\nnot quote"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_unordered_lists(self):
+        block = "- one\n- two"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+
+        block = "- one\n* two"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_ordered_lists(self):
+        block = "1. one\n2. two"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+
+        block = "1. one\n3. three"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
