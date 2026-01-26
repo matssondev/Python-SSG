a66ed992793201e4985b86693a62427d02017ee2
matssondev <patric@matsson.dev>
2026-01-26 20:23:51 +0100

    cleanup

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_directory(source, destination):
+   shutil.copytree(source, destination)
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    src = "./static"
+    dest = "./public"
+
+    print("Deleting public directory...")
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+
+    print("Copying static files to public directory...")
+    copy_directory(src, dest)
 
 if __name__ == "__main__":
     main()
